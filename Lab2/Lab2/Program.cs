717fe16bc7a157037079aa512237b9c2c1a1c9e5
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-10-27 19:59:34 -0400

    Lab 2

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}\n{1}", "Hello World!", "From Ryan Kaufman");
+
+        }
+    }
+}
