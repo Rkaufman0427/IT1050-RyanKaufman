41c528979221726ba5e2e4adeff7c4d70970bff7
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-10-27 21:08:03 -0400

    Lab2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -8,10 +8,9 @@ namespace Lab2
         {
             Console.WriteLine("{0}\n{1}", "Hello World!", "From Ryan Kaufman");
 
-            Console.WriteLine("Hello World");
-            Console.WriteLine("");
-            Console.WriteLine("From Ryan Kaufman");
-
+            Console.WriteLine("Hello World!  From Ryan Kaufman");
+            Console.WriteLine(" A. At what point in the program does our C# Console Application execute?, The point of execution is the Main Method.");
+            Console.WriteLine(" B. What is the difference between an integer type variable and a double / floating-point variable?, The Difference is that Integer Type are whole numbers and Double / Floating are Decimals.");
         }
     }
 }
