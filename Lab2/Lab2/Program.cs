796046e5ed129bcd8d3eccc886ca4d447aab271b
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-10-28 20:19:11 -0400

    Lab2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -4,16 +4,16 @@ namespace Lab2
 {
     class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args )
         {
             int number1 = 15;
             int number2 = 12;
             int sum = 27;
 
-            Console.Write("Enter first integer:15");
+            Console.Write("Enter first integer:15"); // prompt user// read first number from user
             number1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter second integer:12");
+            Console.Write("Enter second integer:12"); // prompt user// read second number from user
             number2 = Convert.ToInt32(Console.ReadLine());
 
             sum = number1 + number2;
