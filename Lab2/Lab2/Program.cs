7385ca882e90475c616f492b8c7bfc5215f2a3d9
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-01 19:43:54 -0500

    Lab2

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -19,16 +19,16 @@ namespace Lab2
             sum = number1 + number2;
 
             Console.WriteLine("Sum is {0}", sum);
-    
+        
             Console.WriteLine("{0}\n{1}", "Hello World!", "From Ryan Kaufman");
 
             Console.WriteLine("Hello World! From Ryan Kaufman");
 
-            Console.WriteLine("A. At what point in our program does our C# Console Applications execute?, It would be the main entry point"):
-            Console.WriteLine("B. What is the difference between an integer type variable and a double / floating-point variable?, The Differance is that Integer Type are whole number and Double/Floating-point variables are decimals.");
+            Console.WriteLine("A. At what point in our program does our C# Console Applications execute?, It would be the main entry point");
+            Console.WriteLine("B. What is the difference between an integer type variable and a double / floating-point variable?, The Difference is that Integer Type are whole number and Double/Floating-point variables are decimals.");
             Console.WriteLine("C. We can create blocks of code that we can call by name using a method.  Give an example of a method from Chapter 4., an example of a Method is SetName");
             Console.WriteLine("D. Like we have types double and int, we can create our own custom types using Classes.  Each class has variables called instance variables or class members.  To read the value of an instance variable, we create method called a GET accessor.  To assign a value ton instance variable, we use a method called a SET accessor.");
-            Console.WriteLine("E. What is the difference between a class and an object?  How many instances of a class can we create (please explain your answer)?")
+            Console.WriteLine("E. What is the difference between a class and an object?, A class is a template for objects.  How many instances of a class can we create?, There is no limit you can create as many as you want.");
         }
     }
 }
