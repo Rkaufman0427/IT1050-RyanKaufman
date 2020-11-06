cbf6e0017d287fb33780a52020fabf8a6dfa29dc
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-05 19:03:09 -0500

    Lab3

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The If Single- Selection Statement and the While Repetition Statement are the same because they both specify and action for a result and they are different because the While Repetition Statement allows the application to repeat itself and the If Single-Selection Statment does not");
+        }
+    }
+}
