53218fff925462f14ba318010450fcbb63202e1c
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-06 12:10:40 -0500

    Lab3

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -6,38 +6,26 @@ namespace Lab3
     {
         public static void Main(string[] args)
         {
-            // Problem 2 
+            // Problem 
             Console.WriteLine("The If Single- Selection Statement and the While Repetition Statement are the same because they both specify and action for a result and they are different because the While Repetition Statement allows the application to repeat itself and the If Single-Selection Statment does not");
 
             // Problem 3
             int speedLimit;
             int speed;
 
-            speedLimit = 48;
+            speedLimit = 35;
             speed = 42;
 
-            if (speed > (speedLimit * 2))
+            if (speed > speedLimit)
             {
-                Console.WriteLine("Reckless Endangerment!!");
-            }
-            else if (speed > (speedLimit + 10))
-            {
-                Console.WriteLine("Slow Down!!");
-            }
-            else if (speed > speedLimit)
-            {
-                Console.WriteLine("Slow Down");
-            }
-            else
-            {
-                Console.WriteLine("Good driving.");
+            Console.WriteLine("Slow Down");
             }
 
             // Problem 4
             //int isTrue;
             //isTrue = 1;
             bool isTrue;
-            isTrue = false;
+            isTrue = true;
 
             if (isTrue)
             {
@@ -48,13 +36,9 @@ namespace Lab3
                 Console.WriteLine("It is False!");
             }
 
-            int a = 10;
+            int a = 2;
             if (a < 5)
                 Console.WriteLine("It is True");
-
-
-            // Problem 5
-
 
             Console.ReadLine();
         }
