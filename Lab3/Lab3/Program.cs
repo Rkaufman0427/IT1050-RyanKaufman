39b023e99846ac036c94a900eba0b269ed650463
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-05 20:51:19 -0500

    Lab 3

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -4,9 +4,26 @@ namespace Lab3
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("The If Single- Selection Statement and the While Repetition Statement are the same because they both specify and action for a result and they are different because the While Repetition Statement allows the application to repeat itself and the If Single-Selection Statment does not");
+
+            string speedLimit = Console.ReadLine();
+            string speed = Console.ReadLine();
+
+            string message = "";
+
+            if (speedLimit == "35")
+            {
+                message = "";
+                Console.WriteLine(message);
+            }
+            else if (speed == "42")
+            {
+                message = "SLOW NOW!";
+                Console.WriteLine(message);
+            }
+            Console.ReadLine();
         }
     }
 }
