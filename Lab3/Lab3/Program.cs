0469f478933ad8b05921fe120fbda14b08a1d498
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-06 10:26:20 -0500

    Lab3

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -2,37 +2,23 @@
 
 namespace Lab3
 {
-    public class Driving
+    class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("The If Single- Selection Statement and the While Repetition Statement are the same because they both specify and action for a result and they are different because the While Repetition Statement allows the application to repeat itself and the If Single-Selection Statment does not");
 
-            int speedLimit = 35;
-            int speed = 42;
+            int speedLimit;
+            int speed;
+
+            speedLimit = 35;
+            speed = 42;
+
             if (speed > speedLimit)
             {
-             Console.WriteLine("Slow Down");
+            Console.WriteLine("Slow Down");
             }
-             Console.ReadLine();
 
-        }
-    }
-}
-    public class booleantest
-    {
-            private static bool isTrue;
-            private static void Main(string[]args)
-            {
-            bool user = isTrue;
-            if (user == isTrue)
-            {
-                Console.WriteLine("It is True!");
-            }
-            else
-            {
-                Console.WriteLine("It is False!");
-        }
         }
     }
 }
