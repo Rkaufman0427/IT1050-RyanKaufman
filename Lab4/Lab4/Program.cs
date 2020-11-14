3742bf8a296d64f80002cafce55583861328b133
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-14 13:31:01 -0500

    Lab4

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Elements are 1. a control variable or loop counter. 2. The control variables initial value. 3. The control variables increment thats applied during each iteration of the loop. 4. the loop-continuation condition that determines if looping should continue.");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
