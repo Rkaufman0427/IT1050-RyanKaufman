6213d820141c8605449771396ad30a8c4550ee3f
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-23 19:01:26 -0500

    Lab5

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A. Scoop would be Double Height and Double Width");
+            Console.WriteLine("B. This is Non Static because Static is not used before the return type");
+            Console.WriteLine("C. The Return Type is double");
+            Console.WriteLine("D. The Method Name is getArea");
+            Console.WriteLine("E. Parameters are Double Height and Double Width");
+            Console.WriteLine("F. Method Body would be return height * width");
+        }
+    }
+}
