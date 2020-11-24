38a6545ae882a326a6ebdcfc1439d28180afcfbb
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-11-23 19:41:16 -0500

    Lab5

--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -10,8 +10,10 @@ namespace Lab5
             Console.WriteLine("B. This is Non Static because Static is not used before the return type");
             Console.WriteLine("C. The Return Type is double");
             Console.WriteLine("D. The Method Name is getArea");
-            Console.WriteLine("E. Parameters are Double Height and Double Width");
+            Console.WriteLine("E. Parameters are Public");
             Console.WriteLine("F. Method Body would be return height * width");
+            Console.WriteLine("The differance between a User Defined Method and a Method Provided with Framework is that a User Defined Method is written by the user and are hidden from other methods and Frameworks can be used from several locations. When making a User Defined we should not try and REINVENT THE WHEEL");
+            Console.WriteLine("The differance between Static and Non Static is that Non Static has access to all Non Static fields and Static does not have the access to these fields.");
         }
     }
 }
