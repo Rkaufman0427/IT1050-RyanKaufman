7e96c59134f0b774f80b90740c19bedd1ab25f73
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-12-13 16:09:39 -0500

    Lab7

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab7
+{
+    public class Book
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Book Info Example");
+        }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string PublishYear { get; set; }
+        Console.WriteLine();
+
+            }
+    
+        }
