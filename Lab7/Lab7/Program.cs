f39f11c949c925a5e5450765c59df68b68b97287
Ryan Kaufman <S00611956@acad.tri-c.edu>
2020-12-13 17:13:32 -0500

    Lab7

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -2,17 +2,45 @@
 
 namespace Lab7
 {
-    public class Book
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Book Info Example");
         }
-        public string Author { get; set; }
-        public string Title { get; set; }
-        public string PublishYear { get; set; }
-        Console.WriteLine();
+        public string authorfirstName { get; private set; }
+        public string authorlastName { get; private set; }
+        public string Title { get; private set; }
+        public string datePublishYear { get; private set; }
 
-            }
-    
+        public Book(string authorfirstName, string authorlastName, string Title, string datePublishYear)
+        {
+            authorfirstName = "J.R.R.";
+            authorlastName = "Tolkein";
+            Title = "The Lord of the Rings";
+            datePublishYear = "1954";
         }
+        public override string ToString()
+        {
+            return string.Format("Author:, Title:, Published, firstName, lastName, Title, datePublishYear");
+        }
+        class BookTest
+        {
+            public static void Main(string[] args)
+        }
+        var author = new Author("J.R.R. Tolkein");
+        var title = new Title("The Lord of the Rings");
+        var publish year = new /Publish year("1954");
+        BookTest Information = new Book("The Lord of the Rings,"J.R.R.Tolkein,"1954");
+
+        Console.WriteLine("information");
+        {
+        }
+
+            Console.WriteLine("Question 3,CLR is what is caled before adding a constructor");
+            Console.WriteLine("Question 4, The concept of Overloading would be used");
+            Console.WriteLine("Question 5, So fault tolerant programs can resolve exceptions");
+            Console.WriteLine("Question 6, The purpose of private vs. public when adding new properties is so that it can hide implementations from clients. Public can be seen, used and accessed by any emthod whereas private is nly used when called and needed.");
+            Console.WriteLine(")
+    }
+}
